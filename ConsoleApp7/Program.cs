5c334976d309717cfcc3584ad16a02caa8c40f7a
Артур Хисматуллин <OtusBasicGradWork>
2024-11-15 20:04:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Xml;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var value = Environment.GetEnvironmentVariable(variable [, Target])
+        }
+    }
+}
